2fcbee4e885fff8cc901e373c3af5a4262a33f94
Andi <1vandidroid1@gmail.com>
2021-03-23 22:50:26 +0100

    Test

--- src/main/java/com/codecool/whattoprint/Challenge.java
+++ src/main/java/com/codecool/whattoprint/Challenge.java
@@ -2,6 +2,10 @@ package com.codecool.whattoprint;
 
 class Challenge {
     public static String decideAndSay(String[] words, int number) {
-        return words[0];
+        if (number%21 == 0) return words[0] + words[1];
+        if (number%3 == 0) return words[0];
+        if (number%7 == 0) return words[1];
+
+        return "";
     }
 }
